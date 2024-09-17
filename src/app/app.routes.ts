d506bd347d51aa33a250636d1b27827adfbc2946
CARREREOlivier <carrereolivier@yahoo.fr>
2024-09-17 10:01:40 +0200

    router mis à jour

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {AarComponent} from "./aar/aar.component";
+import {LetsPlayComponent} from "./lets-play/lets-play.component";
+import {FanFictionComponent} from "./fan-fiction/fan-fiction.component";
+import {ConnexionComponent} from "./connexion/connexion.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'aar', component: AarComponent },
+  { path: 'lets-play', component: LetsPlayComponent },
+  { path: 'fan-fiction', component: FanFictionComponent },
+  { path: 'connexion', component: ConnexionComponent },
+];
